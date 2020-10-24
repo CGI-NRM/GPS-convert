748e8f276bfc240d876da6129742502d5f65cbc4
Thomas Källman <thomas.kallman@nrm.se>
2020-10-24 11:30:10 +0200

    Add more allowed file formats

--- app.R
+++ app.R
@@ -24,7 +24,7 @@ ui <- fluidPage(
             fileInput("uploaded_file", "Choose file that holds GPS data",
                       multiple = FALSE,
                       accept = c(".xls", ".xlsx", ".ods", ".csv",
-        ".tdf", ".txt")),
+        ".tdf", ".txt", ".tsv")),
             tags$hr(),
             checkboxInput("header", "Header", TRUE),
 
@@ -38,11 +38,11 @@ ui <- fluidPage(
                          choices = c(All = "all", Head = "head"),
                          selected = "all"),
 
-            uiOutput("select_X"),
+            uiOutput("select_N"),
 
             uiOutput("select_Y"),
             
-            uiOutput("select_N")
+            uiOutput("select_X")
         ),
 
         mainPanel(
@@ -52,7 +52,9 @@ ui <- fluidPage(
                                  downloadButton("downloadData2", "Download2")),
                         tabPanel("Converted GPS coordinates",
                                  DT::dataTableOutput("df_conv")),
-                        tabPanel("Map", leafletOutput("map")))        )
+                        tabPanel("Map", leafletOutput("map")))
+
+        )
     )
 )
 
@@ -74,24 +76,26 @@ server <- function(input, output, session) {
     })
 
   # Dynamically generate UI input appears after file is loaded
+
+    output$select_N <- renderUI({
+      selectInput(inputId = "select_N",
+                  label = "Select Column with Sample name",
+                  choices = names(df()))
+    })
+    
+    output$select_Y <- renderUI({
+    selectInput(inputId = "select_Y",
+                       label = "Select Column with Latitude data",
+                       choices = names(df()))
+    })
+
     output$select_X <- renderUI({
     selectInput(inputId = "select_X",
                        label = "Select Column with Longitude data",
                 choices = names(df()))
     })
 
-    output$select_Y <- renderUI({
-    selectInput(inputId = "select_Y",
-                       label = "Select Column with Latitude data",
-                       choices = names(df()))
-    })
     
-    output$select_N <- renderUI({
-      selectInput(inputId = "select_N",
-                  label = "Select Column with Sample name",
-                  choices = names(df()))
-    })
-
     df_sel <- reactive({
       req(input$select_X)
       req(input$select_Y)
@@ -125,6 +129,19 @@ server <- function(input, output, session) {
         p2
     })
 
+  # Generate WGS84 values for map even if other formats are asked for  
+    df_convMap <- reactive({
+        inPutGPS <- REFS[[input$gpsfrom]]
+        outPutGPS <- REFS[["WGS84"]]
+        df <- df_sel()
+        df <- df[,-1]
+        p1 <- SpatialPointsDataFrame(df, data = df_sel(), proj4string = inPutGPS)
+        p2 <- spTransform(p1, outPutGPS)
+        #colnames(p2@coords) <- c(paste("Longitude", input$gpsto, sep = " "), paste("Latitude", input$gpsto, sep = " "))
+        #p2@coords
+        p2
+    })
+    
   # Generate table of converted values
     output$df_conv <- DT::renderDataTable({
         if(input$disp == "head") {
@@ -137,7 +154,7 @@ server <- function(input, output, session) {
 
   # Generate map from converted values
     output$map <- renderLeaflet({
-      leaflet(df_conv()) %>%
+      leaflet(df_convMap()) %>%
           addProviderTiles(providers$OpenStreetMap,
                            options = providerTileOptions(noWrap = TRUE)) %>%
           addMarkers(label = ~htmlEscape(Name))
